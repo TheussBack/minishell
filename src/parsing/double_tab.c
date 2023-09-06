abf78f79b8d81e029e70c17aacc036f19746864f
LoLaparr <louis.laparre@hotmail.com>
2023-09-06 12:39:55 +0200

    il faut fixe make_line

--- src/parsing/double_tab.c
+++ src/parsing/double_tab.c
@@ -18,32 +18,54 @@ static int	count_nb_of_tab(t_types *current)
 	return (nb);
 }
 
-static void	make_line(t_types *current, char **tab, int i)
+static void	nb_of_char(t_types *current, int *end)
+{
+	while (current->type == current->next->type)
+	{
+		current = current->next;
+		*end += 1;
+	}
+}
+
+static void	make_line(t_types *current, char **tab, int i, int nb)
 {
 	int	start;
 	int	end;
 	int	j;
 
 	start = 0;
-	end = 0;
+	end = 1;
 	while (current)
 	{
-		if (current->type == current->next->type)
-			++end;
+		printf("start%i\nend%i\n", start, end);
+		nb_of_char(current, &end);
+		printf("---start%i\n---end%i\n", start, end);
 		if (end > start)
-			tab[i] = malloc(sizeof(char) * (end - start) + 1); // malloc a proteger
+			tab[i] = malloc(sizeof(char) * (end - start) + 1);
+			if (!tab[i])
+				return;
 		j = 0;
 		while (start < end)
 		{
 			tab[i][j] = *((char*)current->valeur);
-			++start;										// seg fault a gerer
+			++start;
 			++j;
-			current = (t_types *)current->next;
+			printf("START=%i\n", j);
+			current = current->next;
 		}
 		tab[i][j] = '\0';
+		//int	a = 0;
+		//while (tab[a])
+		//{
+		//	printf("tab[i] = %s\n", tab[a]);
+		//	++a;
+		//}
 		start = end;
+		++end;
 		++i;
-		write(1, "\n-----\n", 7);
+		printf("___i=%i\n___nb=%i", i, nb);
+		if (i == (nb -1))
+			return ;
 	}
 }
 
@@ -61,7 +83,7 @@ int	create_double_tab(t_types *head)
 		return (1);
 	current = head;
 	i = 0;
-	make_line(current, &tab[i], i);
+	make_line(current, &tab[i], i, nb);
 	tab[i] = NULL;
 
 	int	a = 0;
@@ -72,3 +94,4 @@ int	create_double_tab(t_types *head)
 	}
 	return (0);
 }
+
